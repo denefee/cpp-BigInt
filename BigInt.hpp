06e2193c90dbdacd262c551b5c0f6df87fa6a325
denefee <dsher2004@gmail.com>
2023-11-15 00:53:50 +0300

    fix wrong constructor calls in operator-= and operator+=

--- BigInt.hpp
+++ BigInt.hpp
@@ -1,18 +1,20 @@
-#include <bits/stdc++.h>
-
-using namespace std;
+#include <iostream>
+#include <cstring>
+#include <stdexcept>
+#include <algorithm>
 
 class BigInt{
-	string digits;
+	std::string digits;
+	bool sign = true;
 public:
 	// Constructors:
-	BigInt(unsigned long long n = 0);
-	BigInt(string &);
+	BigInt(int64_t n = 0);
+	BigInt(const std::string&); 
 	BigInt(const char *);
-	BigInt(BigInt &);
+	BigInt(const BigInt&);
 
 	// Helper functions:
-	friend int Length(const BigInt &);
+	friend int Length(const BigInt &); // | size_t , а вообще можно было сделать методом класса
 	int operator[](const int)const;
 
 	// Operator overloading
@@ -38,70 +40,82 @@ public:
 	friend BigInt &operator*=(BigInt &, const BigInt &);
     friend BigInt operator*(const BigInt &, const BigInt &);
 
-	friend ostream &operator<<(ostream &,const BigInt &);
-	friend istream &operator>>(istream &, BigInt &);
+	friend std::ostream &operator<<(std::ostream &,const BigInt &);
+	friend std::istream &operator>>(std::istream &, BigInt &);
 };
 
-BigInt::BigInt(string &s) {
-	string digits = "";
-	int n = s.size();
-	for (int i = 0; i < n; i++) {
-		if(!isdigit(s[i]))
-			throw("type error: not a number");
+BigInt::BigInt(const std::string &s) {
+	int p = 0;
+	if (s[0] == '-') {
+		sign = false;
+		p++;
+	} 
+	for (int i = p; i < s.size(); i++) {
+		if(!isdigit(s[i])) {
+			throw std::invalid_argument("Oops2");
+		}
 		digits.push_back(s[i] - '0');
 	}
 }
 
-BigInt::BigInt(unsigned long long number) {
-	string temp = "";
+BigInt::BigInt(int64_t number) {
+	std::string temp = "";
+	if (number < 0) {
+		sign = false;
+		number = -number;
+	}
+
 	while (number) {
 		temp.push_back(number % 10);
 		number /= 10;
 	}
-	for (int i = temp.size()-1; i >= 0; i--) {
-		digits.push_back(temp[i]);
-	}
+
+	std::reverse(temp.begin(), temp.end());
+	digits = temp;
 }
 
 BigInt::BigInt(const char *s) {
-	digits = "";
-	for (int i = 0; i < strlen(s); i++)
-	{
-		if(!isdigit(s[i]))
-			throw("type error: not a number");
-		digits.push_back(s[i] - '0');
+	digits = s;
+	if (s[0]=='-') {
+		sign = false;
+		s++;
+	}
+	while(*s) {
+		if (!isdigit(*s)) throw std::invalid_argument("Oops1");
+		s++;
 	}
 }
 
-BigInt::BigInt(BigInt &a) {
-	digits = a.digits;
-}
+BigInt::BigInt(const BigInt &a): digits(a.digits), sign(a.sign) {}
 
 int Length(const BigInt &a) {
 	return a.digits.size();
 }
 
 int BigInt::operator[](const int index) const {
-	if(digits.size() <= index || index < 0)
+	if (digits.size() <= index || index < 0)
 		throw("error: out of range");
 	return digits[index];
 }
 
 bool operator==(const BigInt &a, const BigInt &b) {
-	return a.digits == b.digits;
+	return (a.digits == b.digits)&&(a.sign == b.sign);
 }
 
-bool operator!=(const BigInt & a, const BigInt &b) {
+bool operator!=(const BigInt &a, const BigInt &b) {
 	return !(a == b);
 }
 
 bool operator<(const BigInt&a, const BigInt&b) {
+	if (a.sign != b.sign)
+		return !(a.sign);
+
 	int n = Length(a), m = Length(b);
-	if(n != m)
-		return n < m;
-	while(n--)
-		if(a.digits[n] != b.digits[n])
-			return a.digits[n] < b.digits[n];
+	if (n != m)
+		return (n < m)&&(a.sign);
+	for (int i = 0; i < n; i++)
+		if(a.digits[i] != b.digits[i])
+			return a.digits[i] < b.digits[i];
 	return false;
 }
 
@@ -119,25 +133,37 @@ bool operator<=(const BigInt&a, const BigInt&b) {
 
 BigInt &BigInt::operator=(const BigInt &a) {
 	digits = a.digits;
+	sign = a.sign;
 	return *this;
 }
 
 BigInt &BigInt::operator++() {
-	int n = digits.size();
-	for (int i = n-1; i >= 0; i--) {
-		if (digits[i] == 9) {
-			digits[i] = 0;
-			if (i == 0) {
-				digits = '1' + digits;
+	if (sign) {
+		int n = digits.size();
+		for (int i = n-1; i >= 0; i--) {
+			if (digits[i] == 9) {
+				digits[i] = 0;
+				if (i == 0) {
+					digits = '1' + digits;
+					break;
+				}
+			}
+			else {
+				digits[i]++;
 				break;
 			}
 		}
-		else {
-			digits[i]++;
-			break;
-		}
+		return *this;
 	}
-	return *this;
+	else {
+		BigInt temp = BigInt(digits);
+		temp--;
+		if ((Length(temp) == 1)&&(temp[0] == 0))
+			temp.sign = true;
+		digits = temp.digits;
+		sign = temp.sign;
+		return *this;
+	}
 }
 
 BigInt BigInt::operator++(int num) {
@@ -148,16 +174,27 @@ BigInt BigInt::operator++(int num) {
 }
 
 BigInt &BigInt::operator--() {
-    if (digits[0] == 0 && digits.size() == 1)
-        throw("error: underflow");
+	if (sign) {
+		if (digits[0] == 0 && digits.size() == 1) {
+			digits[0] = 1;
+			sign = false;
+		}
 
-    int i, n = digits.size();
-    for (i = n-1; digits[i] == 0 && i >= 0; i--)
-        digits[i] = 9;
-    digits[i]--;
-    if (n > 1 && digits[n - 1] == 0)
-        digits.pop_back();
-    return *this;
+		int i, n = digits.size();
+		for (i = n-1; digits[i] == 0 && i >= 0; i--)
+			digits[i] = 9;
+		digits[i]--;
+		if (n > 1 && digits[n - 1] == 0)
+			digits.pop_back();
+		return *this;
+	}
+
+	else {
+		BigInt temp = BigInt(digits);
+		temp++;
+		digits = temp.digits;
+		return *this;
+	}
 }
 
 BigInt BigInt::operator--(int num) {
@@ -168,21 +205,48 @@ BigInt BigInt::operator--(int num) {
 }
 
 BigInt &operator+=(BigInt &a, const BigInt& b) {
-	int t = 0, s, i;
-	int n = Length(a), m = Length(b);
-	if (m > n)
-		a.digits.append(m - n, 0);
-	n = Length(a);
-	for (i = 0; i < n; i++){
-		if (i < m)
-			s = (a.digits[i] + b.digits[i]) + t;
-		else
-			s = a.digits[i] + t;
-		t = s / 10;
-		a.digits[i] = (s % 10);
+	BigInt a_tmp = a;
+	BigInt b_tmp = b;
+	
+	a_tmp.sign = true;
+	b_tmp.sign = true;
+
+	if ((!b.sign)&&(!a.sign)) {
+		a_tmp += b_tmp;
+		a.digits = a_tmp.digits;
+		return a;
 	}
-	if (t)
-		a.digits.push_back(t);
+
+	if ((a.sign)&&(!b.sign)) {
+		a -= b_tmp;
+		return a;
+	}
+
+	if ((!a.sign)&&(b.sign)) {
+		b_tmp-= a_tmp;
+		a.digits = b_tmp.digits;
+		a.sign = b_tmp.sign;
+		return a;
+	}
+
+	if ((b.sign)&&(a.sign)) {
+		int t = 0, s, i;
+		int n = Length(a), m = Length(b);
+		if (m > n)
+			a.digits.append(m - n, 0);
+		n = Length(a);
+		for (i = 0; i < n; i++){
+			if (i < m)
+				s = (a.digits[i] + b.digits[i]) + t;
+			else
+				s = a.digits[i] + t;
+			t = s / 10;
+			a.digits[i] = (s % 10);
+		}
+		if (t)
+			a.digits.push_back(t);
+		return a;
+	}
 	return a;
 }
 
@@ -194,31 +258,58 @@ BigInt operator+(const BigInt &a, const BigInt &b) {
 }
 
 BigInt &operator-=(BigInt&a, const BigInt &b) {
-	if (a < b)
-		throw("error: underflow");
-	int n = Length(a), m = Length(b);
-	int i, t = 0, s;
-	for (i = 0; i < n; i++){
-		if(i < m)
-			s = a.digits[i] - b.digits[i]+ t;
-		else
-			s = a.digits[i]+ t;
-		if(s < 0)
-			s += 10,
-			t = -1;
-		else
-			t = 0;
-		a.digits[i] = s;
+	BigInt a_tmp = a;
+	BigInt b_tmp = b;
+	a_tmp.sign = b_tmp.sign = true;
+
+	if ((a.sign)&&(!b.sign)) {
+		a += b_tmp;
+		return a;
 	}
-	while (n > 1 && a.digits[n - 1] == 0)
-		a.digits.pop_back(),
-		n--;
+
+	if ((!a.sign)&&(b.sign)) {
+		a_tmp += b_tmp;
+		a.digits = a_tmp.digits;
+		return a;
+	}
+
+	if ((!a.sign)&&(!b.sign)) {
+		b_tmp -= a_tmp;
+		a = std::move(b_tmp);
+		return a;
+	}
+
+	if ((a.sign)&&(b.sign)) {        
+		if (a < b) {
+			b_tmp -= a_tmp;
+			a.digits = std::move(b_tmp.digits);
+			a.sign = false;
+			return a;
+		}
+		int n = Length(a), m = Length(b);
+		int i, t = 0, s;
+		for (i = 0; i < n; i++){
+			if(i < m)
+				s = a.digits[i] - b.digits[i]+ t;
+			else
+				s = a.digits[i]+ t;
+			if(s < 0)
+				s += 10,
+				t = -1;
+			else
+				t = 0;
+			a.digits[i] = s;
+		}
+		while (n > 1 && a.digits[n - 1] == 0)
+			a.digits.pop_back(),
+			n--;
+		return a;
+	}
 	return a;
 }
 
 BigInt operator-(const BigInt& a, const BigInt&b) {
-	BigInt temp;
-	temp = a;
+	BigInt temp = a;
 	temp -= b;
 	return temp;
 }
@@ -247,6 +338,12 @@ BigInt &operator*=(BigInt &a, const BigInt &b) {
     while (result.digits.size() > 1 && result.digits.back() == 0) {
         result.digits.pop_back();
     }
+
+	if (a.sign == b.sign)
+		result.sign = true;
+	else 
+		result.sign = false;
+
     a = result;
     return a;
 }
@@ -258,20 +355,23 @@ BigInt operator*(const BigInt &a, const BigInt &b) {
     return temp;
 }
 
-istream &operator>>(istream &in, BigInt&a){
-	string s;
+std::istream &operator>>(std::istream &in, BigInt&a){
+	std::string s;
 	in >> s;
-	int n = s.size();
-	for (int i = 0; i < n; i++) {
-		if(!isdigit(s[i]))
-			throw("type error: not a number");
-		a.digits.push_back(s[i] - '0');
-	}
+	a = BigInt(s);
 	return in;
 }
 
-ostream &operator<<(ostream &out, const BigInt &a){
-	for (int i = 0; i < a.digits.size(); i++)
-		out << (short)a.digits[i];
-	return out;
+std::ostream &operator<<(std::ostream &out, const BigInt &a){
+	if (a.sign) {
+		for (int i = 0; i < a.digits.size(); i++)
+			out << (short)a.digits[i];
+		return out;
+	}
+	else {
+		out << '-';
+		for (int i = 0; i < a.digits.size(); i++)
+			out << (short)a.digits[i];
+		return out;
+	}
 }
--- testing.cpp
+++ testing.cpp
@@ -1,50 +1,63 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <cstring>
+#include <stdexcept>
+#include <algorithm>
 #include "BigInt.hpp"
 
-using namespace std;
-
 void test_of_length() {
-    BigInt n("12345");
+    BigInt n(12345);
     int l = Length(n);
-    if (l == 5) {cout << "CORRECT\n";}
-    else {cout << "ERROR\n";} 
+    if (l == 5) {std::cout << "CORRECT\n";}
+    else {std::cout << "ERROR\n";}   // | На эту тему разбирали вариант самописного макроса
+                                // | похожего на REQUIRE из Catch2
+                                // | Или можно было взять прям Catch2
 }
 
 void test_of_streams() {
-    cout << "Print your number: ";
+    std::cout << "Print your number: ";
     BigInt num;
-    cin >> num;
-    cout << "Your number is: "<< num << '\n';
+    std::cin >> num;
+    std::cout << "Your number is: "<< num << '\n';
 }
 
 void test_of_plus_plus() {
-    BigInt num("12345");
+    BigInt num(12345);
     num++;
-    if (num == 12346) {cout << "CORRECT\n";}
-    else {cout << "ERROR\n";} 
+    if (num == 12346) {std::cout << "CORRECT\n";}
+    else {std::cout << "ERROR\n";} 
 }
 
 BigInt test_of_summ() {
-    BigInt n1("234");
-    BigInt n2("432");
+    BigInt n1(234);
+    BigInt n2(432);
 
     BigInt n;
-    n = n1 + n2;
+    n = n1 + n2; 
     
     return n; 
 }
 
 void test_of_minus_minus() {
-    BigInt num("12345");
+    BigInt num(12345);
     num--;
-    if (num == 12344) {cout << "CORRECT\n";}
-    else {cout << "ERROR\n";} 
+    std::cout << num << '\n';
+    if (num == 12344) {std::cout << "CORRECT\n";}
+    else {std::cout << "ERROR\n";} 
 }
 
 BigInt test_of_substract() {
-    BigInt n1("54321");
-    BigInt n2("43210");
+    BigInt n1(54321);
+    BigInt n2(43210);
+
+    BigInt n;
+    n = n1 - n2;
+    
+    return n; 
+}
+
+BigInt test_of_substract_only_negatives() {
+    BigInt n1(-12345);
+    BigInt n2(-12345);
 
     BigInt n;
     n = n1 - n2;
@@ -53,109 +66,113 @@ BigInt test_of_substract() {
 }
 
 void test_of_equality() {
-    BigInt n1("12345");
-    BigInt n2("12345");
-    if (n1 == n2) {cout << "CORRECT\n";}
-    else {cout << "ERROR\n";} 
+    BigInt n1(12345);
+    BigInt n2(12345);
+    if (n1 == n2) {std::cout << "CORRECT\n";}
+    else {std::cout << "ERROR\n";} 
 }
 
 void test_of_inequality() {
-    BigInt n1("12345");
-    BigInt n2("12346");
-    if (n1 != n2) {cout << "CORRECT\n";}
-    else {cout << "ERROR\n";} 
+    BigInt n1(12345);
+    BigInt n2(12346);
+    if (n1 != n2) {std::cout << "CORRECT\n";}
+    else {std::cout << "ERROR\n";} 
 }
 
 void test_of_bigger() {
-    BigInt n1("12345");
-    BigInt n2("123");
-    if (n1 > n2) {cout << "CORRECT\n";}
-    else {cout << "ERROR\n";} 
+    BigInt n1(12345);
+    BigInt n2(123);
+    if (n1 > n2) {std::cout << "CORRECT\n";}
+    else {std::cout << "ERROR\n";} 
 }
 
 void test_of_eqbigger() {
-    BigInt n1("12345");
-    BigInt n2("123");
+    BigInt n1(12345);
+    BigInt n2(123);
 
-    if (n1 >= n2 && n1 >= n1) {cout << "CORRECT\n";}
-    else {cout << "ERROR\n";} 
+    if (n1 >= n2 && n1 >= n1) {std::cout << "CORRECT\n";}
+    else {std::cout << "ERROR\n";} 
 }
 
 void test_of_smaller() {
-    BigInt n1("12345");
-    BigInt n2("123");
-    if (n2 < n1) {cout << "CORRECT\n";}
-    else {cout << "ERROR\n";} 
+    BigInt n1(12345);
+    BigInt n2(123);
+    if (n2 < n1) {std::cout << "CORRECT\n";}
+    else {std::cout << "ERROR\n";} 
 }
 
 void test_of_eqsmaller() {
-    BigInt n1("123");
-    BigInt n2("12345");
+    BigInt n1(123);
+    BigInt n2(12345);
 
-    if (n1 <= n2 && n1 <= n1) {cout << "CORRECT\n";}
-    else {cout << "ERROR\n";} 
+    if (n1 <= n2 && n1 <= n1) {std::cout << "CORRECT\n";}
+    else {std::cout << "ERROR\n";} 
 }
 
 void test_of_mult() {
-    BigInt a("12");
-    BigInt b("13");
+    BigInt a(-12);
+    std::cout << a << '\n';
+    BigInt b(13);
     BigInt res;
     res = a*b;
+    std::cout << res << '\n';
 
-    if (res == 156) {cout << "CORRECT\n";}
-    else {cout << "ERROR\n";} 
+    if (res == -156) {std::cout << "CORRECT\n";}
+    else {std::cout << "ERROR\n";} 
 }
 
 void test_of_giantism() {
-    BigInt huge("1");
+    BigInt huge(1);
     for (int i = 0; i < 64; i++) huge*=2;
     BigInt a, b;
     a = (huge - 1)*(huge + 1);
     b = huge * huge;
 
-    if (b - a == 1) {cout << "CORRECT\n";}
-    else {cout << "ERROR\n";} 
+    if (b - a == 1) {std::cout << "CORRECT\n";}
+    else {std::cout << "ERROR\n";} 
 }
 
 
 int main() {
     test_of_streams();
 
-    cout << "Testing of Length() result: ";
+    std::cout << "Testing of Length() result: ";
     test_of_length();
 
-    cout << "Testing of operator== result: ";
+    std::cout << "Testing of operator== result: ";
     test_of_equality();
 
-    cout << "Testing of operator!= result: ";
+    std::cout << "Testing of operator!= result: ";
     test_of_inequality();
 
-    cout << "Testing of operator> result: ";
+    std::cout << "Testing of operator> result: ";
     test_of_bigger();
 
-    cout << "Testing of operator>= result: ";
+    std::cout << "Testing of operator>= result: ";
     test_of_eqbigger();
 
-    cout << "Testing of operator< result: ";
+    std::cout << "Testing of operator< result: ";
     test_of_smaller();
 
-    cout << "Testing of operator<= result: ";
+    std::cout << "Testing of operator<= result: ";
     test_of_eqsmaller();
 
-    cout << "Testing of operator++ result: ";
+    std::cout << "Testing of operator++ result: ";
     test_of_plus_plus();
 
-    cout << "Testing of operator+ result: 234 + 432 = " << test_of_summ() << '\n';
+    std::cout << "Testing of operator+ result: 234 + 432 = " << test_of_summ() << '\n';
 
-    cout << "Testing of operator-- result: ";
+    std::cout << "Testing of operator-- result: ";
     test_of_minus_minus();
 
-    cout << "Testing of operator- result: 54321 - 43210 = " << test_of_substract() << '\n';
+    std::cout << "Testing of operator- result: 54321 - 43210 = " << test_of_substract() << '\n';
 
-    cout << "Testing of operator* result: ";
+    std::cout << "Testing of operator- with equal negatives result: -12345 - (-12345) = " << test_of_substract_only_negatives() << '\n';
+
+    std::cout << "Testing of operator* result: ";
     test_of_mult();
 
-    cout << "Testing of giantism result: ";
+    std::cout << "Testing of giantism result: ";
     test_of_giantism();
     
 
